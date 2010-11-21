b549377b19de29836193a5de1db028872ad53f86
Gennadiy Zlobin <gennad.zlobin@gmail.com>
2010-11-21 19:02:50 +0600

    TrayIcon added

--- src/com/gennadz/Main.java
+++ src/com/gennadz/Main.java
@@ -1,15 +1,22 @@
 package com.gennadz;
 
+import java.awt.AWTException;
 import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,9 +25,15 @@ public class Main {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable(){
 			public void run() {
-				MainFrame frame = new MainFrame();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
+				MainFrame frame;
+				try {
+					frame = new MainFrame();
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.setVisible(true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
@@ -29,8 +42,10 @@ public class Main {
 class MainFrame extends JFrame {
 	public static final int DEFAULT_WIDTH = 300;
 	public static final int DEFAULT_HEIGHT = 200;
+	private SystemTray systemTray = SystemTray.getSystemTray();
+	private TrayIcon trayIcon;
 	
-	public MainFrame() {
+	public MainFrame() throws IOException {
 		setTitle("This is header");
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		//buttons
@@ -42,6 +57,38 @@ class MainFrame extends JFrame {
 		//creating action to button
 		OpenSiteAction openSiteAction = new OpenSiteAction();
 		siteButton.addActionListener(openSiteAction);
+		//tray icon
+		trayIcon = new TrayIcon(ImageIO.read(new File("trayicon.gif")), "Tray test");
+		trayIcon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(true);
+				setState(JFrame.NORMAL);
+				removeTrayIcon();
+			}
+	    });
+		addWindowStateListener(new WindowStateListener()
+	    {
+			@Override
+			public void windowStateChanged(WindowEvent e) {
+				if(e.getNewState() == JFrame.ICONIFIED) {
+					setVisible(false);
+					addTrayIcon();
+				}
+			}
+	    });
+	}
+	
+	private void removeTrayIcon() {
+		systemTray.remove(trayIcon);
+	}
+	private void addTrayIcon() {
+		try {
+			systemTray.add(trayIcon);
+			trayIcon.displayMessage("Tray test", "Window minimised to tray, double click to show", TrayIcon.MessageType.INFO);
+	    }
+	    catch(AWTException ex) {
+	      ex.printStackTrace();
+	    }
 	}
 }
 
